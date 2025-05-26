e07494e19ae920c174f3da79f1bcf5951c0cbc64
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-05-26 18:21:19 -0300

    inicializando repo

---
+++ CapacitaBrasil-RollBound/CapacitaBrasil_RollBoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapacitaBrasil_RollBoundApp.swift
+//  CapacitaBrasil-RollBound
+//
+//  Created by Izadora Montenegro on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CapacitaBrasil_RollBoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

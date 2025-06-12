4ffb6f1f2a623edfa46a63c9c6c4d306f3fbd8b3
Izadora Montenegro <125757162+izamontenegro@users.noreply.github.com>
2025-06-12 15:47:23 -0300

    feat: implementa componente de textfield

---
+++ CapacitaBrasil-RollBound/View/Components/TextField.swift
@@ -0,0 +1,33 @@
+//
+//  TextField.swift
+//  CapacitaBrasil-RollBound
+//
+//  Created by Izadora de Oliveira Albuquerque Montenegro on 06/06/25.
+//
+
+import SwiftUI
+
+struct SinglelineTextfield: View {
+    @State var fieldName: String
+    @Binding var input: String
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            TextField(fieldName, text: $input)
+                .foregroundStyle(Color.AppColors.unactive)
+                .font(.custom("Sora", size: 21))
+                .fontWeight(.bold)
+            Rectangle()
+                .foregroundStyle(Color.AppColors.unactive)
+                .frame(maxWidth: .infinity, maxHeight: 2)
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var input: String = ""
+    @Previewable @State var fieldName: String = "Nome do personagem "
+    SinglelineTextfield(fieldName: fieldName, input: $input)
+        .padding(.horizontal)
+}

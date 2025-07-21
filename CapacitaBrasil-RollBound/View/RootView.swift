06b13483aab36095d482172d61a24bf3c3922c34
Izadora Montenegro <125757162+izamontenegro@users.noreply.github.com>
2025-07-21 19:36:51 -0300

    feat: implementa onboarding com TabView e altera ícone do app

--- CapacitaBrasil-RollBound/View/RootView.swift
+++ CapacitaBrasil-RollBound/View/RootView.swift
@@ -8,26 +8,42 @@
 import SwiftUI
 
 struct RootView: View {
+    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     @State private var selectedTab: Tab = .dices
     @ObservedObject var rollViewModel = RollViewModel.shared
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Group {
-                switch selectedTab {
-                case .initiative:
-                    InitiativeView()
-                case .dices:
-                    DicesView()
-                case .characters:
-                    CharactersView()
+        if hasSeenOnboarding {
+            ZStack(alignment: .bottom) {
+                Group {
+                    switch selectedTab {
+                    case .initiative:
+                        InitiativeView()
+                    case .dices:
+                        DicesView()
+                    case .characters:
+                        CharactersView()
+                    }
+                }
+                
+                if rollViewModel.state == .idle {
+                    CustomTabBar(selectedTab: $selectedTab)
                 }
             }
-
-            if rollViewModel.state == .idle {
-                CustomTabBar(selectedTab: $selectedTab)
+        } else {
+            ZStack {
+                Color.AppColors.primary.ignoresSafeArea()
+                TabView {
+                    OnboardingWelcomeView()
+                    OnboardingInitiativeView()
+                    OnboardingDiceView()
+                    OnboardingCharactersView()
+                    OnboardingStartView()
+                }
+                .tabViewStyle(PageTabViewStyle())
             }
         }
+        
     }
 }
 
